0e81e0fe184398f674aaa6d696d3696ada1e21ba
omar saade <omarsaadeh11@gmail.com>
2022-06-01 22:58:05 +0300

    react project

--- reactsection8/src/components/Users/UsersList.js
+++ reactsection8/src/components/Users/UsersList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from '../UI/Card';
 
+import classes from './UsersList.module.css'
+
 
 
 const UsersList = (props) => {
     return (
-        <Card>
+        <Card className={classes.users} >
             <ul>
                 {props.users.map((user) => (
                     <li>
