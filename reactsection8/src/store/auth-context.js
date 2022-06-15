f10ccaa908613e66bf37d9bde56728913eb2bcb7
omar saade <omarsaadeh11@gmail.com>
2022-06-15 22:40:52 +0300

    react

--- reactsection8/src/store/auth-context.js
+++ reactsection8/src/store/auth-context.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-//AuthContext is ana object that contain a component
+//AuthContext is an object that contain a component
+// This default value will actually only be used if we would consume without having a provider.
 const AuthContext = React.createContext({
     isLoggedIn: false //default value
 });
 
 export default AuthContext;
+
+// This default value will actually only be used if we would consume without having a provider.
