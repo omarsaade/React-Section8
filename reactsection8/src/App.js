bfaa15e902628d6ba007309d3c9cfe368141a5b1
omar saade <omarsaadeh11@gmail.com>
2022-06-16 21:40:19 +0300

    react

--- reactsection8/src/App.js
+++ reactsection8/src/App.js
@@ -8,7 +8,7 @@ import AuthContext from './store/auth-context';
 
 function App() {
   const ctx = useContext(AuthContext);
-  // console.log(ctx);
+
   return (
     <Fragment>
       <MainHeader />
